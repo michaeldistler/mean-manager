2a6f0d11bc5ab7329b80b3ad85a60a2bacf5c825
Michael Distler <michaeldistler@gmail.com>
2016-04-10 22:18:26 -0400

    Modified manage.js

--- manage.js
+++ manage.js
@@ -3,14 +3,17 @@ var createProject = require('./commands/create-project.js');
 
 var args = process.argv;
 
-
-switch(args[2]) {
-    case "create-module":
-        createModule.createModule();
-        break;
-    case "create-project":
-        createProject.createProject();
-        break;
-    default:
-        console.log(args[2] + " is not a commmand");
+var manage = function() {
+    switch(args[2]) {
+        case "create-module":
+            createModule.createModule();
+            break;
+        case "create-project":
+            createProject.createProject();
+            break;
+        default:
+            console.log(args[2] + " is not a commmand");
+    }
 }
+
+module.exports = manage();
